79ca4131ad14c1cab47907c430ed2a06afe5dde5
Tai-Yen Chen <vaiogt@gmail.com>
2013-09-16 16:42:40 +0800

    save() using item class.

--- hw2/public/javascripts/index.js
+++ hw2/public/javascripts/index.js
@@ -4,17 +4,31 @@ var tmpl = '<li><input type="text"><span></span></li>';
 var insert = function(content){
   var newLi = $(tmpl).prependTo('.main');
   if(content !== undefined){
-    newLi.find('span').text(content);
+    // newLi.find('span').text(content);
+    newLi.find('span').text(content.text);
+
+    if (content.status) {
+      newLi.addClass("is-done");
+    };
+
   }else{ // 增加新的空 <li>，應為編輯模式
     newLi.addClass('is-editing');
+    newLi.append($("<button>確認</button>"));
     newLi.find('input').focus();
   }
 };
 
 var save = function(){
   var arr = []; // 要存的陣列
-  $('ul').find('span').each(function(){
-    arr.unshift($(this).text());
+
+  $("ul").find("li").each(function(){
+    var item = {};
+    item.text = $(this).find("span").text();
+    if($(this).hasClass("is-done")){
+      item.status = true;
+    }
+
+    arr.unshift(item);
   });
   
   console.log('saving', arr);
@@ -22,14 +36,19 @@ var save = function(){
 };
 
 var load = function(){
+
+  $("ul").empty();
+
   var arr;
   if(localStorage.todoItems){
     arr = JSON.parse(localStorage.todoItems);        
     
     console.log('loading', arr);
+    
     arr.forEach(function(content){
       insert(content);
     });
+
   }
 };
 
@@ -39,8 +58,8 @@ $('#add').click(function(){
 
 $('ul').on('keyup', 'input', function(e){
   if(e.which === 13){
-    var input = $(e.target),
-        li = input.parents('.is-editing');
+    var input = $(e.target);
+    var li = input.parents('.is-editing');
     
     li.find('span').text(input.val());
     li.removeClass('is-editing');
@@ -51,8 +70,8 @@ $('ul').on('keyup', 'input', function(e){
 load();
 
 // 課堂練習一：
-var sortableLists = $('.connected'),
-    placeholder = $('#placeholder');
+var sortableLists = $('.connected');
+var placeholder = $('#placeholder');
 
 sortableLists.sortable({
   connectWith: '.connected',
@@ -68,6 +87,19 @@ sortableLists.on('sortstart', function(){
 
 // 課堂練習三：
 $('.delete').on('sortreceive', function(e, ui){
+  // alert(ui.item.find("span").text());
   ui.item.remove();
   save();
+});
+
+//  homework2 - drag <li> to div.done
+$(".done").on("sortreceive", function(event, ui){
+  ui.item.addClass("is-done");
+  save();
+  load();
+});
+
+//  homework2 - click on any area to complete input
+$("body").on("click", function(){
+  alert("not-editing");  
 });
