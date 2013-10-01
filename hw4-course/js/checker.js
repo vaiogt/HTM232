6e710cf1a22b6257d3cfec0627d91f8a44e2ba17
Tai-Yen Chen <vaiogt@gmail.com>
2013-10-01 21:52:53 +0800

    homework 4 done.

--- hw4-course/js/checker.js
+++ hw4-course/js/checker.js
@@ -1,13 +1,12 @@
-window.fbAsyncInit = function(){
-  FB.init({
-    appId: '229034627174596',
-    status: true,
-    cookie: true
-  });
+window.fbAsyncInit = function() {
+	FB.init({
+		appId : '229034627174596',
+		status : true,
+		cookie : true
+	});
 };
 
-var startButton = $('.hw4-start-button'),
-    results = $('.hw4-result');
+var startButton = $('.hw4-start-button'), results = $('.hw4-result');
 
 // 垃圾社團列表
 var junkGroups = [];
@@ -16,10 +15,10 @@ var junkGroups = [];
 // 用 jsonp 自 http://antispam.tonyq.org/groups/jsonp/?jsonp=parser 取得列表，但有注意事項
 // Server 端要吃一個參數支援 jsonp
 $.ajax("http://spamgroup.tonyq.org/groups/jsonp", {
-	dataType: "jsonp",
-	jsonp: "jsonp", // server 端參數名稱
-	success: function(data){
-		data.forEach(function(record){
+	dataType : "jsonp",
+	jsonp : "jsonp", // server 端參數名稱
+	success : function(data) {
+		data.forEach(function(record) {
 			junkGroups.push(record.GID);
 		});
 		startButton.removeAttr("disabled").removeClass("disabled");
@@ -33,27 +32,45 @@ $.ajax("http://spamgroup.tonyq.org/groups/jsonp", {
 //   startButton.removeAttr('disabled').removeClass('disabled');
 // }
 
-
 // 1. 讓使用者登入此 Facebook App (FB.login)
 // 2. 以 FB.api 拿到使用者的 group 列表
 // 3. 比對每個使用者的 group 是否有在 junkGroups 中出現
 //
-startButton.click(function(){
-  results.empty(); // 清除裡面的內容
-  $('.hw4-complete').remove(); // 移除「掃描完成」
+startButton.click(function() {
+	results.empty();
+	// 清除裡面的內容
+	$('.hw4-complete').remove();
+	// 移除「掃描完成」
 
+	// Facebook login
+	// https://developers.facebook.com/docs/reference/javascript/FB.login/
+	FB.login(function(response){
+		// console.log(response);
+		// 實際上需要有if判斷是否有登入
+		// https://developers.facebook.com/docs/reference/javascript/FB.api/
+		FB.api("/me/groups", function(resp){
+			// console.log(resp);
+			var groups = resp.data;
+			groups.forEach(function(group){
+				if (junkGroups.indexOf(group.id) != -1) {
+					results.append("<tr><th>" + group.id + "</th><td>" + group.name + "</td>");
+				};
+			});
+			
+			results.after("<div class='hw4-complete alert alert-info'>掃描完成！！</div>");
+		});
+	}, {scope: "user_groups"});
+	// 拿到使用者 group 列表的 response 之後：
+	// function(resp){
+	//   var groups = resp.data;
+	//   groups.forEach(function(group){
+	//     console.log(group);
+	//     if( junkGroups.indexOf(group.id) !== -1 ){
+	//       results.append('<tr><th>' + group.id + '</th><td>' + group.name + '</td>');
+	//     }
+	//   });
 
-  // 拿到使用者 group 列表的 response 之後：
-  // function(resp){
-  //   var groups = resp.data;
-  //   groups.forEach(function(group){
-  //     console.log(group);
-  //     if( junkGroups.indexOf(group.id) !== -1 ){
-  //       results.append('<tr><th>' + group.id + '</th><td>' + group.name + '</td>');
-  //     }
-  //   });
+	//   results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
+	// }
 
-  //   results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
-  // }
-
-});
+}); 
